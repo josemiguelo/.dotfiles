e903f64afddc3b5aa2eedd11b26dc3de56a37c01
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2025-10-31 10:12:45 -0500

    fix autocmds not firing at the right time

--- private_dot_config/nvim/lua/features/neovim_core/wins.lua
+++ private_dot_config/nvim/lua/features/neovim_core/wins.lua
@@ -5,45 +5,47 @@ local get_window_by_ft = require("util.windows").get_window_by_ft
 --[[
   add more filetypes to close with q
 --]]
-local suffix = "close_with_q"
-delete_augroup(suffix) -- remove original command
-vim.api.nvim_create_autocmd("FileType", {
-  group = create_augroup(suffix),
-  pattern = {
-    "PlenaryTestPopup",
-    "checkhealth",
-    "dbout",
-    "gitsigns-blame",
-    "grug-far",
-    "help",
-    "lspinfo",
-    "neotest-output",
-    "neotest-output-panel",
-    "neotest-summary",
-    "notify",
-    "qf",
-    "spectre_panel",
-    "startuptime",
-    "tsplayground",
-    "git",
-    "man",
-    "scratch",
-    "fugitive",
-  },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.schedule(function()
-      vim.keymap.set("n", "q", function()
-        vim.cmd("close")
-        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
-      end, {
-        buffer = event.buf,
-        silent = true,
-        desc = "Quit buffer",
-      })
-    end)
-  end,
-})
+LazyVim.on_very_lazy(function()
+  local suffix = "close_with_q"
+  delete_augroup(suffix) -- remove original command
+  vim.api.nvim_create_autocmd("FileType", {
+    group = create_augroup(suffix),
+    pattern = {
+      "PlenaryTestPopup",
+      "checkhealth",
+      "dbout",
+      "gitsigns-blame",
+      "grug-far",
+      "help",
+      "lspinfo",
+      "neotest-output",
+      "neotest-output-panel",
+      "neotest-summary",
+      "notify",
+      "qf",
+      "spectre_panel",
+      "startuptime",
+      "tsplayground",
+      "git",
+      "man",
+      "scratch",
+      "fugitive",
+    },
+    callback = function(event)
+      vim.bo[event.buf].buflisted = false
+      vim.schedule(function()
+        vim.keymap.set("n", "q", function()
+          vim.cmd("close")
+          pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+        end, {
+          buffer = event.buf,
+          silent = true,
+          desc = "Quit buffer",
+        })
+      end)
+    end,
+  })
+end)
 
 -- TODO: jdtls toggle functionality
 
