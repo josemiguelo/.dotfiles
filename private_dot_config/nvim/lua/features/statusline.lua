59ab7e5be1648196a62873306e51b1a2f2d3f050
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2025-11-26 14:13:57 -0500

    enhance lualine sections

--- private_dot_config/nvim/lua/features/statusline.lua
+++ private_dot_config/nvim/lua/features/statusline.lua
@@ -1,6 +1,15 @@
+local function enhance_section(opts, section, feature)
+  opts.sections["lualine_" .. section] = {
+    { feature, color = { gui = "bold,italic" } },
+  }
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   opts = function(_, opts)
+    enhance_section(opts, "a", "mode")
+    enhance_section(opts, "b", "branch")
+
     table.insert(opts.sections.lualine_x, 1, "filetype")
     opts.sections.lualine_z = {}
   end,
