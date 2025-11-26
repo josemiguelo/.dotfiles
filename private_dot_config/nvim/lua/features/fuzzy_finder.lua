17b01fad18480310a13cf8dbac7749c21917656b
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2025-11-26 11:00:18 -0500

    better <space><space> for finding files

--- private_dot_config/nvim/lua/features/fuzzy_finder.lua
+++ private_dot_config/nvim/lua/features/fuzzy_finder.lua
@@ -47,10 +47,14 @@ return {
     keys = {
       { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
       { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+
       { "<leader>fR", LazyVim.pick("oldfiles"), desc = "Recent" },
       { "<leader>fr", function() Snacks.picker.recent({ filter = { cwd = true }}) end, desc = "Recent (cwd)" },
+
       { "<leader>sG", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
       { "<leader>sg", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+
+      { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
     },
   },
   {
