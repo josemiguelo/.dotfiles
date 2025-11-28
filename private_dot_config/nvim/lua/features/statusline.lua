6f149f1f2d7ae3e5e38953081c212d9ee7c41242
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2025-11-28 12:45:14 -0500

    make bold some other lualine sections

--- private_dot_config/nvim/lua/features/statusline.lua
+++ private_dot_config/nvim/lua/features/statusline.lua
@@ -1,16 +1,15 @@
-local function enhance_section(opts, section, feature)
-  opts.sections["lualine_" .. section] = {
-    { feature, color = { gui = "bold,italic" } },
-  }
-end
-
 return {
   "nvim-lualine/lualine.nvim",
   opts = function(_, opts)
-    enhance_section(opts, "a", "mode")
-    enhance_section(opts, "b", "branch")
+    opts.sections.lualine_a = { { "mode", color = { gui = "bold" } } }
+    opts.sections.lualine_b = { { "branch", color = { gui = "bold" } } }
 
     table.insert(opts.sections.lualine_x, 1, "filetype")
+
+    opts.sections.lualine_y = {
+      { "progress", color = { gui = "bold" }, separator = " ", padding = { left = 1, right = 0 } },
+      { "location", color = { gui = "bold" }, padding = { left = 0, right = 1 } },
+    }
     opts.sections.lualine_z = {}
   end,
 }
