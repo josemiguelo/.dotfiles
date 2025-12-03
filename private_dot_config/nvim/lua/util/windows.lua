db10129486d818f97d30fae39bd2b950c4836cbd
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2025-12-03 13:30:43 -0500

    refactor

--- private_dot_config/nvim/lua/util/windows.lua
+++ private_dot_config/nvim/lua/util/windows.lua
@@ -1,17 +1,17 @@
 local M = {}
 
 function M.get_window_by_ft(ft)
-  local fugitive_win = nil
+  local win_ret = nil
 
   for _, win in ipairs(vim.api.nvim_list_wins()) do
     local buf = vim.api.nvim_win_get_buf(win)
     local filetype = vim.api.nvim_get_option_value("filetype", { buf = buf })
     if filetype == ft then
-      fugitive_win = win
+      win_ret = win
     end
   end
 
-  return fugitive_win
+  return win_ret
 end
 
 return M
