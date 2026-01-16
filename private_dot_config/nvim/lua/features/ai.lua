e8e804f3ebb1007eac9506ea231a80f10fb7c718
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2026-01-16 11:51:10 -0500

    enable copilot

--- private_dot_config/nvim/lua/features/ai.lua
+++ private_dot_config/nvim/lua/features/ai.lua
@@ -2,7 +2,6 @@ return {
   {
     "folke/sidekick.nvim",
     opts = {
-      nes = { enabled = false },
       cli = {
         mux = {
           enabled = true,
@@ -11,78 +10,6 @@ return {
         win = {
           split = {
             width = 120, -- set to 0 for default split width
-          },
-        },
-      },
-    },
-    -- stylua: ignore
-    keys = {
-      { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
-      {
-        "<c-.>",
-        function() require("sidekick.cli").toggle() end,
-        desc = "Sidekick Toggle",
-        mode = { "n", "t", "i", "x" },
-      },
-      {
-        "<leader>aa",
-        function() require("sidekick.cli").toggle() end,
-        desc = "Sidekick Toggle CLI",
-      },
-      {
-        "<leader>as",
-        function() require("sidekick.cli").select() end,
-        desc = "Select CLI",
-      },
-      {
-        "<leader>ad",
-        function() require("sidekick.cli").close() end,
-        desc = "Detach a CLI Session",
-      },
-      {
-        "<leader>at",
-        function() require("sidekick.cli").send({ msg = "{this}" }) end,
-        mode = { "x", "n" },
-        desc = "Send This",
-      },
-      {
-        "<leader>af",
-        function() require("sidekick.cli").send({ msg = "{file}" }) end,
-        desc = "Send File",
-      },
-      {
-        "<leader>av",
-        function() require("sidekick.cli").send({ msg = "{selection}" }) end,
-        mode = { "x" },
-        desc = "Send Visual Selection",
-      },
-      {
-        "<leader>ap",
-        function() require("sidekick.cli").prompt() end,
-        mode = { "n", "x" },
-        desc = "Sidekick Select Prompt",
-      },
-    },
-  },
-
-  {
-    "folke/snacks.nvim",
-    optional = true,
-    opts = {
-      picker = {
-        actions = {
-          sidekick_send = function(...)
-            return require("sidekick.cli.picker.snacks").send(...)
-          end,
-        },
-        win = {
-          input = {
-            keys = {
-              ["<a-a>"] = {
-                "sidekick_send",
-                mode = { "n", "i" },
-              },
-            },
           },
         },
       },
