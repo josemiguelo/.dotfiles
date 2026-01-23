6b82fe46bd2beb799a3b52e9785d1da2d3293f2e
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2026-01-22 23:36:23 -0500

    add oil to nvim

--- private_dot_config/nvim/lua/features/fuzzy_finder.lua
+++ private_dot_config/nvim/lua/features/fuzzy_finder.lua
@@ -86,4 +86,24 @@ return {
       },
     },
   },
+  {
+    "stevearc/oil.nvim",
+    cmd = "Oil",
+    opts = {
+      columns = {
+        "icon",
+        { "mtime", highlight = "Comment", format = "%T %y-%m-%d" },
+      },
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+      },
+      float = {
+        padding = 3,
+        win_options = {
+          winblend = 0,
+        },
+      },
+    },
+  },
 }
