e2fd777aa9adad8abb950e6392a9a04050348b26
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2026-04-10 18:19:05 -0500

    add kotlin lsp to neovim

--- private_dot_config/nvim/lua/features/langs/kotlin.lua
+++ private_dot_config/nvim/lua/features/langs/kotlin.lua
@@ -5,4 +5,39 @@ return {
       ensure_installed = { "kotlin" },
     },
   },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = { kotlin_lsp = { enabled = false } },
+    },
+  },
+
+  {
+    "AlexandrosAlexiou/kotlin.nvim",
+    ft = { "kotlin" },
+    dependencies = {
+      "mason-lspconfig.nvim",
+      "oil.nvim",
+      "trouble.nvim",
+      {
+        "mason.nvim",
+        opts = { ensure_installed = { "kotlin-lsp" } },
+      },
+    },
+    config = function()
+      require("kotlin").setup({
+        root_markers = {
+          "settings.gradle",
+          "settings.gradle.kts",
+          "build.xml",
+          "pom.xml",
+          "build.gradle",
+          "build.gradle.kts",
+        },
+
+        jdk_for_symbol_resolution = vim.env.HOME .. "/.asdf/installs/java/adoptopenjdk-21.0.6+7.0.LTS",
+      })
+    end,
+  },
 }
