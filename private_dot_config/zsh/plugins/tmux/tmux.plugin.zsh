81f67e1dc8732b3ca06da34f62df9042355f42ee
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2026-09-17 14:56:34 -0500

    add kitty keybinding for tmux-attach

--- private_dot_config/zsh/plugins/tmux/tmux.plugin.zsh
+++ private_dot_config/zsh/plugins/tmux/tmux.plugin.zsh
@@ -9,3 +9,13 @@ alias tmuxconf='$EDITOR $ZSH_TMUX_CONFIG'
 alias tr="tmuxinator"
 alias tre="tmuxinator edit"
 alias trs="tmuxinator start"
+
+# C-M-a runs the picker outside tmux; inside tmux the root-table binding in
+# tmux.conf takes the same key first, so kitty can send it either way
+_tmux_attach_widget() {
+  zle push-line
+  BUFFER='tmux-attach'
+  zle accept-line
+}
+zle -N _tmux_attach_widget
+bindkey '^[^A' _tmux_attach_widget
