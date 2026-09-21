512c9778802e10ff185022c8df3286c21a5bc072
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2026-09-21 11:28:29 -0500

    prepare dotfiles for omedora-4

--- private_dot_config/zsh/plugins/brew/brew.plugin.zsh
+++
@@ -1,4 +0,0 @@
-# still load brew since it can be used on several platforms
-# `brew shellenv` is a bash script and costs ~25ms to fork; its output only
-# changes when brew itself does, so serve it from a cache
-[[ -x /opt/homebrew/bin/brew ]] && zcache brew /opt/homebrew/bin/brew shellenv
